560db58d8ec4b196c2d16579d732f0329dddad00
Kanata-chan <septaraditya64@gmail.com>
2023-03-01 16:50:17 +0700

    Kanata-chan

---
+++ itsme.js
@@ -0,0 +1,6 @@
+const nama = "Deff";
+const asal = "Deff, Jawa Barat";
+const keahlian = ["JavaScript", "HTML", "C++"];
+const umur = 16;
+const pacar = "Nggak ada pacar;)";
+console.log(`Perkenalkan, nama saya ${nama}. Saya berasal dari ${asal}. Saya memiliki keahlian di bidang ${keahlian.join(", ")}. Saat ini saya berusia ${umur} tahun dan pacar saya bernama ${pacar}.`);
